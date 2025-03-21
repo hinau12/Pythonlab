26d892244f6fb4ef934d83f64fcbdc8a55adeb5e
hinau12 <naazhina144@gmail.com>
2025-03-21 16:28:36 +0530

    Create distance.py

---
+++ week1/ci.py/distance.py
@@ -0,0 +1,9 @@
+import math
+x1=float(input("enter x1 co-ordinates"))
+y1=float(input("enter y1 co-ordinates"))
+x2=float(input("enter x2 co-ordinates"))
+y2=float(input("enter y2 c0-ordinates"))
+b=x2-x1
+h=y2-y1
+d=math.sqrt((b**2)+(h**))
+print("distance =",d)
